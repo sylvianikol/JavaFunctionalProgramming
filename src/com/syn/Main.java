54ac05f660bf8ff81b084dc74f19b6ecc01660c0
sylvianikol <silviaadvert@gmail.com>
2021-04-22 09:09:05 +0300

    Higher Order Functions examples

--- src/com/syn/Main.java
+++ src/com/syn/Main.java
@@ -1,8 +1,31 @@
 package com.syn;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<String> list = new ArrayList<>();
+        list.add("dsfsf");
+        list.add("abc");
+        list.add("bbb");
+
+        Collections.sort(list, (a, b) -> {
+            return a.compareTo(b);
+        });
+
+        Comparator<String> comparator = (a, b) -> {
+            return a.compareTo(b);
+        };
+
+        Comparator<String> reversedComparator = comparator.reversed();
+
+        Collections.sort(list, reversedComparator);
+
+        System.out.println(list);
     }
 }
